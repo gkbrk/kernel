acabbaab713f628e3432b7626da13fd52ee0e565
Gokberk Yaltirakli <git@gkbrk.com>
2020-06-17 02:26:10 +0100

    fs: Create filesystem interface

---
+++ kernel/fs/FS.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../libk/String.h"
+
+namespace Kernel::FS {
+
+struct DirEntry {
+  String path;
+  size_t size;
+};
+
+class FS {
+public:
+  virtual bool open(String path, String flags) = 0;
+  virtual bool readDir(String path, void (*function)(DirEntry)) = 0;
+};
+
+} // namespace Kernel::FS
