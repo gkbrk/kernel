adc8fbb86b08771875aa55ce8d08dd68fb384e5f
Gokberk Yaltirakli <git@gkbrk.com>
2021-02-26 01:48:12 +0300

    random: Collect entropy and mix into random bytes

--- kernel/Random.cpp
+++ kernel/Random.cpp
@@ -1,5 +1,33 @@
-#include "Random.h"
+#include <kernel/Random.h>
+#include <libk/debug.h>
 #include <stddef.h>
+
+namespace Kernel::Random {
+
+static uint64_t s_dvb2_hash = 5381;
+static float s_entropy_estimate = 0;
+
+static uint8_t s_buf[4096];
+static size_t s_iread = 0;
+static size_t s_iwrite = 0;
+static size_t s_elems = 0;
+
+void feed_data(uint8_t byte, float entropy_estimate) {
+  s_dvb2_hash = ((s_dvb2_hash << 5) + s_dvb2_hash) + byte;
+  s_entropy_estimate += entropy_estimate;
+
+  if (s_entropy_estimate > 64) {
+    uint8_t compressed =
+        (s_dvb2_hash & 0x000000FF) ^ (s_dvb2_hash & 0x0000FF00) ^
+        (s_dvb2_hash & 0x00FF0000) ^ (s_dvb2_hash & 0xFF000000);
+    dbg() << "Random number: " << compressed;
+    s_entropy_estimate -= 64;
+  }
+}
+
+uint8_t get_byte() { return 0; }
+
+} // namespace Kernel::Random
 
 namespace Kernel {
 
--- kernel/Random.h
+++ kernel/Random.h
@@ -1,6 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+namespace Kernel::Random {
+
+void feed_data(uint8_t, float);
+uint8_t get_byte();
+
+} // namespace Kernel::Random
+
 namespace Kernel {
 void prng_fill(uint8_t *, size_t);
 
