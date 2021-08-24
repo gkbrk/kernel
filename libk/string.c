34a50729b9b3dd77be37c17e0911361b588d684a
Gokberk Yaltirakli <git@gkbrk.com>
2021-08-24 23:48:02 +0300

    toolchain: Compile certain builtins without LTO to fix linker errors
    GCC needs to have implementations of `memset`, `memcpy` and similar
    functions that are compiled without LTO in order to use certain
    optimizations without throwing linker errors.

---
+++ libk/string.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+// NOTICE: This file needs to compiled without LTO (link time optimizations).
+
+// This file includes some libc built-ins with the correct signatures and
+// behaviour. This is kept separate from the rest of the codebase because
+// it is only meant to be used in a specific way.
+
+// When the kernel is compiled with optimizations, GCC emits calls to `memset`,
+// `memcpy` and similar functions when it detects a pattern that does the same
+// thing. Even if you have these functions in your codebase, they won't be used
+// if you are compiling with LTO. Having them in a separate file that doesn't
+// have LTO enabled fixes this problem.
+
+void *memset(void *bufptr, int value, size_t size) {
+    unsigned char *buf = (unsigned char *)bufptr;
+    for (size_t i = 0; i < size; i++)
+        buf[i] = (unsigned char)value;
+    return bufptr;
+}
