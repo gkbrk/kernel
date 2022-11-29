1ccccde1771a5183ed5ca189eeb77e14a063e7cf
Gokberk Yaltirakli <git@gkbrk.com>
2022-11-29 23:35:00 +0300

    libk: Fix compiler warnings for Option.h

--- libk/Option.h
+++ libk/Option.h
@@ -1,32 +1,37 @@
-#pragma once
+#ifndef CARDINAL_LIBK_OPTION_H
+#define CARDINAL_LIBK_OPTION_H
 
 #include <libk/assert.h>
 
 template <typename T> class Option {
 public:
-  explicit Option(T val) {
-    m_has_value = true;
-    m_val = val;
-  }
+  Option() : m_has_value(false) {}
 
-  Option() { m_has_value = false; }
+  explicit Option(T value) : m_has_value(true), m_val(value) {}
 
-  T or_default(T val) const {
-    if (m_has_value) {
-      return m_val;
-    } else {
-      return val;
-    }
-  }
-
-  T value() const {
+  T unwrap() const {
     ASSERT(m_has_value);
     return m_val;
   }
 
+  T unwrap_or(T def) const {
+    T result = def;
+
+    if (m_has_value) {
+      result = m_val;
+    }
+
+    return result;
+  }
+
+  [[deprecated("Use unwrap instead")]] T value() const { return unwrap(); }
+
   [[nodiscard]] bool is_some() const { return m_has_value; }
+  [[nodiscard]] bool is_none() const { return !m_has_value; }
 
 private:
+  T m_val = {};
   bool m_has_value;
-  T m_val = {0};
 };
+
+#endif
