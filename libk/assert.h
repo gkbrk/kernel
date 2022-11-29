7f507ce2a8e5494104ccf04bf96d8cfdddbdbca9
Gokberk Yaltirakli <git@gkbrk.com>
2022-11-29 23:36:00 +0300

    libk: Use BasicSerial for assertions
    Previously, assertion outputs could be lost due to buffering on
    SerialWriter. This commit changes the assert.h header to write
    directly using BasicSerial instead.

--- libk/assert.h
+++ libk/assert.h
@@ -1,17 +1,15 @@
 #pragma once
 
 #include <arch/x86/idt.h>
-#include <kernel/drivers/serial.h>
+#include <kernel/drivers/BasicSerial.h>
 #include <stdbool.h>
 
 #define ASSERT(x)                                                              \
   do {                                                                         \
     bool val = static_cast<bool>(x);                                           \
     if (!val) {                                                                \
-      serial_force_unlock();                                                   \
-      serial_lock();                                                           \
-      serial_writestring("ASSERTION FAILED\n");                                \
-      serial_printf("%s:%s:%d\n", __func__, __FILE__, __LINE__);               \
+      basic_serial_write_cstr("ASSERTION FAILED\n");                           \
+      basic_serial_printf("%s:%s:%d\n", __func__, __FILE__, __LINE__);         \
       interrupt_disable();                                                     \
       hlt();                                                                   \
     }                                                                          \
