f6863c003b0530f1807d3e01d233fd9e26c4abb4
Gokberk Yaltirakli <git@gkbrk.com>
2022-12-25 17:04:13 +0300

    shell: mem command now outputs memory utilization

--- kernel/shellutils/mem.cpp
+++ kernel/shellutils/mem.cpp
@@ -1,10 +1,14 @@
 #include <kernel/drivers/terminal.h>
-#include <libk/debug.h>
+#include <kernel/memory/PageFrameAllocator.h>
 #include <libk/printf.h>
 
 void shell_mem(char *) {
-  dbg() << "Memory usage: " << getMemUsage() << " bytes";
+  size_t page_frame_used = get_page_frame_allocator()->allocatedPages() * 4096;
+  size_t mem_usage_kb = page_frame_used / 1000;
+
+  basic_serial_printf("[MemUsage] Physical mem usage is %d KB\n", mem_usage_kb);
+
   Kernel::Drivers::VGATerminal::lock();
-  kprintf("Memory usage: %d bytes\n", getMemUsage());
+  kprintf("Memory usage: %d KB\n", mem_usage_kb);
   Kernel::Drivers::VGATerminal::unlock();
 }
